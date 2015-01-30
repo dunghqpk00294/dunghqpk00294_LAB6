6159cdb5cb5500b6190e52debd3ad8ae32f81dcd
dunghqpk00294 <dunghqpk00294@fpt.edu.vn>
2015-01-30 14:10:53 +0700

    first commit

---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        MessageBox.Show("dunghqpk00294", "Lab6")
+    End Sub
+End Class
